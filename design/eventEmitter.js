7c8fa1e6ade501726e859389117615272a9c062b
berlinen <1292393108@qq.com>
2021-03-20 23:09:52 +0800

    chore: eventEmitter;

--- design/eventEmitter.js
+++ design/eventEmitter.js
@@ -1,56 +1,46 @@
-class EventEmitter {
-  constructor () {
-    this.eventMap = {};
+class EventEmitters {
+  constructor (maxListeners) {
+    this.events = {};
+    this.maxListeners = maxListeners || 10;
   }
-  /**
-   *
-   * @param {*} type 订阅名称
-   * @param {*} handler  =订阅处理函数
-   */
-  on (type, handler) {
-    if(!(handler instanceof Function)) {
-      throw new Error('handler must be a functon type');
+
+  on(type, cb) {
+    if(!this.events[type]) {
+      this.events[type] = []
     }
-    if(!this.eventMap[type]) {
-      this.eventMap[type] = [];
+    if(this.events[type].length >= this.maxListeners) {
+      console.warn(`${event} length max is ${this.maxListeners}`)
+      return this;
     }
-    this.eventMap[type].push(handler)
+    this.events[type].push(cb);
+    return this
   }
-  /**
-   *
-   * @param {*} type 触发 名称
-   * @param {*} params 执行函数参数
-   */
-  emit(type, params) {
-    if(this.eventMap[type]) {
-      this.eventMap[type].forEach(handler => handler(params));
+
+  emit(type, ...args) {
+    const cbs = this.events[type];
+
+    if(!cbs) {
+      console.warn(`has no this type func`)
+    } else {
+      cbs.forEach(func => func.apply(this, args))
     }
   }
-  /**
-   *
-   * @param {*} type 移除订阅名称
-   * @param {*} handler
-   */
-  off(type, handler) {
-    if(this.eventMap[type]) {
-      this.eventMap[type].spice(this.eventMap[type].indexOf(handler) >>> 0, 1);
+
+  off (type, cb) {
+    if(!cb) {
+      console.warn(`${cb} is required`)
+    } else {
+      this.events[type] = this.events[type].filter(func => func !== cb);
     }
+    return this
+  }
+
+  once(type, cb) {
+    const func = (...args) => {
+      this.off(type, cb);
+      cb.apply(this, args);
+    }
+    this.on(type, func);
+    return this;
   }
 }
-
-// 下面我们对 myEventEmitter 进行一个简单的测试，创建一个 myEvent 对象作为 myEventEmitter 的实例，然后针对名为 “test” 的事件进行监听和触发：
-
-// 实例化组件
-const myEvent = new EventEmitter();
-
-// 写一个简单的handler
-
-const testHandler = function (params) {
-  console.log(`test事件被触发了，testHandler 接收到的入参是${params}`);
-}
-
-// 监听test事件
-myEvent.on("test", testHandler);
-//  在触发 test 事件的同时，传入希望 testHandler 感知的参数
-myEvent.emit("test", 'newState');
-
