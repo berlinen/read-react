266f40240584cf3941a6fab02e7dc8016566ee56
berlinen <1292393108@qq.com>
2021-03-26 00:18:03 +0800

    feat: router

--- about-source/about-router/router/src/App.js
+++ about-source/about-router/router/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+
+function Home() {
+  return  (
+    <div>Home</div>
+  )
+}
+
+function About() {
+  return  (
+    <div>Abouts</div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Link to="/home">home</Link>
+        <Link to="/about">about</Link>
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
